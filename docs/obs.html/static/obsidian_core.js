29a9b82791504691e8341b755d003e57f7e3caea
Christian Alberto Palma Almendariz <cpalma@usfq.edu.ec>
2023-10-17 11:12:24 -0500

    generic change

--- docs/obs.html/static/obsidian_core.js
+++ docs/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/NotasCursos";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '318110106161336306764889019746699783941'                       // used to check whether the localStorage data is stale
+var gzip_hash = '195873108250466139902713131003155356149'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
